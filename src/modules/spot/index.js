658e520397b9a337e2ce0c88958ee288263a0189
Tomachi Anura <info@hbarsuite.network>
2024-07-05 18:17:58 +0200

    added subaccount to export

--- src/modules/spot/index.js
+++ src/modules/spot/index.js
@@ -3,6 +3,7 @@
 module.exports.System = require('./system')
 module.exports.Market = require('./market')
 module.exports.Account = require('./account')
+module.exports.SubAccount = require('./sub-account')
 module.exports.Trade = require('./trade')
 module.exports.Margin = require('./margin')
 
